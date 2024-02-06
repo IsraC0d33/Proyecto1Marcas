0d2752078245df002009baa6bb22e743dd3a341a
Ballwictb <ballwitcb@gmail.com>
2024-02-06 17:51:26 +0100

    changes

--- js/scriptMouse.js
+++ js/scriptMouse.js
@@ -1,20 +1,25 @@
-    //No evaluable -> intensidad:Poca
+//No evaluable -> intensidad:Poca
 
-    let destiny = document.querySelector('.destiny');
-    let foreign = document.querySelector('.foreign');
+let destiny = document.querySelector('.destiny');
+let foreign = document.querySelector('.foreign');
+let project = document.querySelector('.project');
+
+let dashDestinations = document.querySelector('.dash-destinations');
+let dashIntern = document.querySelector('.dash-intern-abroad');
+let dashProjects = document.querySelector('.dash-projects');
+
+destiny.addEventListener('mouseover', () => dashDestinations.style.display = 'flex');
+
+destiny.addEventListener('mouseout', () => dashDestinations.style.display = 'none');
+
+foreign.addEventListener('mouseover', () => dashIntern.style.display = 'flex');
+
+foreign.addEventListener('mouseout', () => dashIntern.style.display = 'none');
 
 
-    let dashDestinations = document.querySelector('.dash-destinations');    
-    let dashIntern = document.querySelector('.dash-intern-abroad');
-    
-    destiny.addEventListener('mouseover', () => dashDestinations.style.display = 'flex');
-  
-    destiny.addEventListener('mouseout', () => dashDestinations.style.display = 'none');
+project.addEventListener('mouseover', () => dashProjects.style.display = 'flex');
 
-    foreign.addEventListener('mouseover', () => dashIntern.style.display = 'flex');
-  
-    foreign.addEventListener('mouseout', () => dashIntern.style.display = 'none');
+project.addEventListener('mouseout', () => dashProjects.style.display = 'none');
 
-  
-  
-    
+
+
