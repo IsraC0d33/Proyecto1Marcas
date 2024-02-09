5469c8fc6c23fb080c0a4cad95ceee153f38b2e3
name <104717038+Ballwictb@users.noreply.github.com>
2024-02-09 12:22:10 +0100

    js

--- js/scriptMouse.js
+++ js/scriptMouse.js
@@ -5,12 +5,17 @@ let foreign = document.querySelector('.foreign');
 let project = document.querySelector('.project');
 let groups = document.querySelector('.groups')
 let resources = document.querySelector('.resources')
+let about = document.querySelector('.about')
+
+
 
 let dashDestinations = document.querySelector('.dash-destinations');
 let dashIntern = document.querySelector('.dash-intern-abroad');
 let dashProjects = document.querySelector('.dash-projects');
 let dashGroups = document.querySelector('.dash-groups')
 let dashResources = document.querySelector('.dash-resources')
+let aboutUs = document.querySelector('.about-us')
+
 
 destiny.addEventListener('mouseover', () => dashDestinations.style.display = 'flex');
 
@@ -25,7 +30,6 @@ project.addEventListener('mouseover', () => dashProjects.style.display = 'flex')
 
 project.addEventListener('mouseout', () => dashProjects.style.display = 'none');
 
-
 groups.addEventListener('mouseover', () => dashGroups.style.display = 'flex');
 
 groups.addEventListener('mouseout', () => dashGroups.style.display = 'none');
@@ -33,6 +37,10 @@ groups.addEventListener('mouseout', () => dashGroups.style.display = 'none');
 resources.addEventListener('mouseover', () => dashResources.style.display = 'flex');
 
 resources.addEventListener('mouseout', () => dashResources.style.display = 'none');
+
+about.addEventListener('mouseover', () => aboutUs.style.display = 'flex');
+
+about.addEventListener('mouseout', () => aboutUs.style.display = 'none');
 
 
 
