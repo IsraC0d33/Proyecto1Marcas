ed80030ba7002e6e145387c7997692e0d685c47d
MarcosAcedoLlerena <150441992+MarcosAcedoLlerena@users.noreply.github.com>
2024-02-17 11:53:09 +0100

    js

--- js/aviso-cookies.js
+++ js/aviso-cookies.js
@@ -2,6 +2,12 @@ const botontAceptarCookies = document.getElementById('btn-aceptar-cookies');
 const avisoCookies = document.getElementById('aviso-cookies');
 const fondoAvisoCokies = document.getElementById('fondo-aviso-cookies');
 
+avisoCookies.classList.add('activo')
+fondoAvisoCokies.classList.add('activo')
+
 botontAceptarCookies.addEventListener('click', () => {
-    
+    avisoCookies.classList.remove('activo');
+    fondoAvisoCokies.classList.remove('activo');
+
+    localStorage.setItem('cookies-aceptadas', true );
 });
