dd89dc2c22c45a21b257b6652fa221d2ae2ad705
MarcosAcedoLlerena <150441992+MarcosAcedoLlerena@users.noreply.github.com>
2024-02-17 11:23:45 +0100

    js

--- js/aviso-cookies.js
+++ js/aviso-cookies.js
@@ -1,2 +1,3 @@
 const botontAceptarCookies = document.getElementById('btn-aceptar-cookies');
 const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCokies = document.getElementById('fondo-aviso-cookies');
