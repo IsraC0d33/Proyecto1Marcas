d24e632bc2689f3b935fb655f413d1588a46bfd3
Ballwictb <ballwitcb@gmail.com>
2024-02-22 11:37:34 +0100

    ➡️Arreglos del rugged👌

--- js/scriptClick.js
+++ js/scriptClick.js
@@ -6,7 +6,7 @@ const countries = container.querySelectorAll('div');
 
 //REVIEW - Creando un objeto para poder añadir los siguientes paises. 
 
-const rugged = [
+const _rugged = [
     {
         esp: () => {
             for (let i = 1; i < countries.length; i++) {
